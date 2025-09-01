336e8d92968ad8b32c28f9877a4e72c2e5a289b2
Alejandro Fenos <alefe53@gmail.com>
2025-08-31 21:24:27 -0300

    vercel popups

--- client/src/views/ChatPopupView.vue
+++ client/src/views/ChatPopupView.vue
@@ -14,7 +14,6 @@ onMounted(() => {
 </script>
 
 <style scoped>
-/* Importamos los estilos globales para que el chat se vea bien */
 @import '../style.css';
 
 .popup-container {
@@ -24,10 +23,9 @@ onMounted(() => {
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  background-color: #1f2937; /* Un color de fondo consistente */
+  background-color: #1f2937; 
 }
 
-/* Hacemos que el componente del chat ocupe todo el espacio disponible */
 .popup-container > :deep(.global-chat-container) {
   flex-grow: 1;
   height: 100%;
